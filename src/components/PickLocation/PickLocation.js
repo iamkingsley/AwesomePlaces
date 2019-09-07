43255864dcd7e587f918133bf484951e92bfc961
Bernard Kingsley Codjoe <cudjoebernard2@gmail.com>
2019-09-07 11:31:48 -0700

    rendering a map

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,17 +1,42 @@
 import React, { Component } from "react";
-import { View, Image, Button, StyleSheet, Text } from "react-native";
+import {StyleSheet, Platform } from "react-native";
+import MapboxGL from '@react-native-mapbox-gl/maps';
 
+MapboxGL.setAccessToken(
+  'pk.eyJ1IjoiYmNvZGpvZSIsImEiOiJjanpvb2pvaHMwNGFkM2JuemQwMTcwMm96In0.Y-MRpWsm2lHBVbXiLKkWnQ',
+);
+ 
 class PickLocation extends Component {
+  state = {
+    pointInView: [-73.970895, 40.723279],
+    zoomLevel: 16,
+  };
+  
+  componentDidMount() {
+    if (Platform.OS === 'android') {
+      MapboxGL.requestAndroidLocationPermissions()
+        .then((ok) => {
+          MapboxGL.locationManager.start();
+        }
+      );
+    }
+  }
+  
+  componentWillUnmount() {
+    MapboxGL.locationManager.dispose();
+  }
+  
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.placeholder}>
-          <Text>Map</Text>
-        </View>
-        <View style={styles.button}>
-          <Button title="Locate Me" onPress={() => alert('Pick Location!')} />
-        </View>
-      </View>
+      <MapboxGL.MapView 
+        ref={c => (this.map = c)} 
+        style={styles.map}
+        >
+       <MapboxGL.Camera
+         zoomLevel={this.state.zoomLevel}
+         centerCoordinate={this.state.pointInView}
+       />
+      </MapboxGL.MapView>
     );
   }
 }
@@ -21,12 +46,9 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center"
   },
-  placeholder: {
-    borderWidth: 1,
-    borderColor: "black",
-    backgroundColor: "#eee",
-    width: "80%",
-    height: 150
+  map: {
+    width: "100%",
+    height: 250
   },
   button: {
     margin: 8
