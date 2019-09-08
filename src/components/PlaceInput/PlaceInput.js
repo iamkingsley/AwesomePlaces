0a81716cd1aeb25addd0a61d4b2437197c8c21c5
Bernard Kingsley Codjoe <cudjoebernard2@gmail.com>
2019-09-08 10:01:10 -0700

    adding validation to placeInput

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import DefaultInput from "../UI/DefaultInput/DefaultInput";
+import DefaultInput from '../UI/DefaultInput/DefaultInput';
 
 const placeInput = props => (
   <DefaultInput
-    placeholder="Place Name"
-    value={props.placeName}
+    placeholder='Place Name'
+    value={props.placeData.value}
+    valid={props.placeData.valid}
+    touched={props.placeData.touched}
     onChangeText={props.onChangeText}
   />
 );
